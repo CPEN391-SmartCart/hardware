9d4a1822b6381f24ed9beeb87dd42f6344f6249b
Tawsif Hasan <tawsifhasan1998@yahoo.com>
2021-04-01 19:17:44 -0700

    Git clean up

--- src/fpga/pathfinding/Queue.sv
+++ src/fpga/pathfinding/Queue.sv
@@ -38,10 +38,21 @@ module Queue_RAM
 	
 	output node_info queue_node
 );
-
-	reg [271:0] mem [MAX_NODES-1:0] /* synthesis ramstyle = "no_rw_check, M10K" */;
+	
 	logic [271:0] read_data;
 	
+	node_mem mem(
+		.clock(clk),
+		.data(write_data),
+		.rdaddress(read_address),
+		.wraddress(write_address),
+		.wren(write_enable),
+		.q(read_data)
+	);
+
+	/*
+	reg [271:0] mem [MAX_NODES-1:0] /* synthesis ramstyle = "no_rw_check, M10K" */
+	/*
 	always_ff @(posedge clk)
 	begin
 		if (write_enable) begin
@@ -49,6 +60,7 @@ module Queue_RAM
 		end
 		read_data <= mem[read_address];
 	end
+	*/
 	
 	assign queue_node = '{read_data[271:256], read_data[255:240], read_data[239:224], read_data[223:208], read_data[207:192], read_data[191:176], read_data[175:160], read_data[159:144], read_data[143:128], read_data[127:112], read_data[111:96], read_data[95:80], read_data[79:64], read_data[63:48], read_data[47:32], read_data[31:16], read_data[15:0]};
 endmodule
