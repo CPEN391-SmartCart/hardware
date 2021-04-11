689a030812639dd578346c9522b60d3ce79ad425
jared-paul <jaredpaul9820@gmail.com>
2021-04-11 05:10:31 -0700

    uncommented pathfidnig

--- project/arm/ARM_SYSTEM/src/pathfinding/dijsktra.h
+++ project/arm/ARM_SYSTEM/src/pathfinding/dijsktra.h
@@ -18,7 +18,7 @@
 #define NEIGHBOUR (volatile unsigned int *)(0xFF200220)
 #define PATH_FINISHED (volatile unsigned int *)(0xFF200240)
 
-//int generate_path(int start_node_id, int goal_node_id, coord_t *path);
+int generate_path(int start_node_id, int goal_node_id, coord_t *path);
 
 
 #endif /* DIJKSTRA_H_ */
