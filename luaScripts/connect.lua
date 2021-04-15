534f40a1bb7413975b6903a032d5496d3ceed00b
Tawsif Hasan <tawsifhasan1998@yahoo.com>
2021-04-14 23:52:13 -0700

    Fix lua folder conflict

--- luaScripts/connect.lua
+++ luaScripts/connect.lua
@@ -1,11 +1,24 @@
--- This information is used by the Wi-Fi dongle to make a wireless connection to the router
--- change ssid and paassword to your wifi details
-station_cfg={}
-station_cfg.ssid="TELUS1949"
-station_cfg.pwd=""
-station_cfg.save=true
+WIFI_SSID = "TELUS1949"
+WIFI_PASSWORD = "8mxhf2z59y"
 
--- configure ESP as a station
+wifi_got_ip_event = function(T)
+  local site = "wikipedia.org"
+  -- Internet connectivity determined with net.dns.resolve().
+  print("WiFi connection is established! IP address is: " .. T.IP)
+  print("DNS server 1: " .. net.dns.getdnsserver(0))
+  print("DNS server 2: " .. net.dns.getdnsserver(1))
+  net.dns.resolve(site, function(sk, ip)
+    if (ip == nil) then
+      print("DNS fail!")
+    else
+      print("Connected to the internet")
+      print(site .. " IP: " .. ip)
+    end
+  end)
+end
+
+wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, wifi_got_ip_event)
+
+print("Connecting to WiFi access point...")
 wifi.setmode(wifi.STATION)
-wifi.sta.config(station_cfg)
-wifi.sta.autoconnect(1)
+wifi.sta.config({ ssid = WIFI_SSID, pwd = WIFI_PASSWORD })
